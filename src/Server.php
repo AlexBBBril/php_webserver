b5ff6546fbe1c30f30f3ff3e8a93ab0335d4563c
Alex <brilalex1989@gmail.com>
2018-05-22 01:03:25 +0300

    create socket, bind socket

--- src/Server.php
+++ src/Server.php
@@ -9,13 +9,35 @@ class Server
     protected $port = null;
     protected $socket = null;
 
+    /**
+     * Server constructor.
+     * @param null $host
+     * @param null $port
+     *
+     * @throws \Exception
+     */
+    public function __construct($host, $port)
+    {
+        $this->host = $host;
+        $this->port = (int) $port;
+
+        $this->createSocket();
+        $this->bind();
+    }
+
+
     protected function createSocket()
     {
         $this->socket = socket_create(AF_INET, SOCK_STREAM, 0);
     }
 
+    /**
+     * @throws \Exception
+     */
     protected function bind()
     {
-
+        if (!socket_bind($this->socket, $this->host, $this->port)) {
+            throw new \Exception('Could not bind: '.$this->host.':'.$this->port.' - '.socket_strerror(socket_last_error()));
+        }
     }
 }
