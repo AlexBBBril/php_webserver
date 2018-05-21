22b7cdf4008ceb807557be07f3c968c6a1846e42
Alex <brilalex1989@gmail.com>
2018-05-22 01:18:27 +0300

    Обработка сокета

--- src/Server.php
+++ src/Server.php
@@ -5,8 +5,8 @@ namespace App\WebServer;
 
 class Server
 {
-    protected $host = null;
-    protected $port = null;
+    protected $host   = null;
+    protected $port   = null;
     protected $socket = null;
 
     /**
@@ -25,8 +25,7 @@ class Server
         $this->bind();
     }
 
-
-    protected function createSocket()
+    protected function createSocket(): void
     {
         $this->socket = socket_create(AF_INET, SOCK_STREAM, 0);
     }
@@ -34,10 +33,36 @@ class Server
     /**
      * @throws \Exception
      */
-    protected function bind()
+    protected function bind(): void
     {
         if (!socket_bind($this->socket, $this->host, $this->port)) {
             throw new \Exception('Could not bind: '.$this->host.':'.$this->port.' - '.socket_strerror(socket_last_error()));
+        }
+    }
+
+    /**
+     * @param callable $callback
+     */
+    public function listen(callable $callback): void
+    {
+        while (true) {
+            socket_listen($this->socket);
+
+            if (false === $client = socket_accept($this->socket)) {
+                socket_close( $client ); continue;
+            }
+
+            // In the real world of course you cannot just fix the max size to 1024..
+            $request = Request::withHeaderString(socket_read( $client, 1024) );
+
+            $response = call_user_func( $callback, $request );
+            if ( !$response || !$response instanceof Response ) {
+                $response = Response::error(404);
+            }
+
+            $response = (string) $response;
+            socket_write( $client, $response, \strlen( $response ) );
+            socket_close( $client );
         }
     }
 }
